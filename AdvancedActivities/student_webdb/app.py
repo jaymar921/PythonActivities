f4507af681dc80cd675b0afe86bf8dff7eb74f72
JayMar <72720429+jaymar921@users.noreply.github.com>
2021-11-10 21:40:14 -0500

    Add files via upload

--- AdvancedActivities/student_webdb/app.py
+++ AdvancedActivities/student_webdb/app.py
@@ -1,49 +1,37 @@
-from flask import Flask,render_template,request,redirect
-from mysql import connector
+from flask import Flask,render_template,request,redirect,url_for
+from database import SAVE_DB,DELETE_STUDENT,GETALL
 
 app = Flask(__name__)
 
-#Connect to database
-database = connector.connect(
-    host = "192.168.1.122",
-    user = "root",
-    password = "",
-    database = 'pythontth'
-)
 
 @app.route("/studentlist")
 def studentlist():
     hlist:list=['#','idno','lastname','firstname','course','level','action']
-    sql:str = f"SELECT * FROM `student`"
-    cursor = database.cursor(dictionary=True)
-    cursor.execute(sql)
-    slist:list = cursor.fetchall()
-    cursor.close()
+    slist = GETALL()
     return render_template("studentlist.html",title="studentlist", studentlist =slist, pageheader = hlist)
 
 @app.route("/deletestudent",methods=["GET"])
 def deletestudent():
-    id:int = request.args.get("id")
-    sql:str = f"DELETE FROM `student` WHERE `id`={id}"
-    cursor = database.cursor(dictionary=True)
-    cursor.execute(sql)
-    database.commit()
-    cursor.close()
+    if DELETE_STUDENT(request.args.get("id")):
+        return redirect(url_for("studentlist",message="Student Removed"))
     return redirect("studentlist")
 
 @app.route("/addstudent",methods=["POST"])
 def addstudent():
-    idno:str = request.form['idno']
-    lastname:str = request.form['lastname']
-    firstname:str = request.form['firstname']
-    course:str = request.form['course']
-    level:str = request.form['level']
-    sql:str = f"INSERT INTO `student` (`idno`,`lastname`,`firstname`,`course`,`level`) VALUE ('{idno}','{lastname}','{firstname}','{course}','{level}')"
-    cursor = database.cursor()
-    cursor.execute(sql)
-    database.commit()
-    cursor.close()
-    return redirect("studentlist")
+    DATA:dict = {
+        "flag":request.form['flag'],
+        "idno":request.form['idno'],
+        "lastname":request.form['lastname'],
+        "firstname":request.form['firstname'],
+        "course":request.form['course'],
+        "level":request.form['level']
+    }
+    flag:int = SAVE_DB(DATA)
+    if flag == 0:
+        return redirect(url_for("studentlist",message="Student Added"))
+    elif flag == 2:
+        return redirect(url_for("studentlist",message="Student Updated"))
+    return redirect(url_for("studentlist",message="Error adding student"))
     
 
 @app.route("/")
---
+++ AdvancedActivities/student_webdb/database.py
@@ -0,0 +1,54 @@
+from mysql import connector
+
+#Connect to database
+DATABASE = connector.connect(
+    host = "192.168.1.122",
+    user = "root",
+    password = "",
+    database = 'pythontth'
+)
+
+
+def SAVE_DB(data:dict)->int:
+	flag:str = data.get("flag")
+	idno:str = data.get("idno")
+	lastname:str = data.get("lastname")
+	firstname:str = data.get('firstname')
+	course:str = data.get("course")
+	level:str = data.get("level")
+	try:
+		sql:str = ""
+		if '-' in flag:
+			sql = f"INSERT INTO `student` (`idno`,`lastname`,`firstname`,`course`,`level`) VALUE ('{idno}','{lastname}','{firstname}','{course}','{level}')"
+		else:
+			sql = f"UPDATE `student` SET idno='{idno}',lastname='{lastname}',firstname='{firstname}',course='{course}',level='{level}' WHERE id='{flag}'"
+		cursor = DATABASE.cursor()
+		cursor.execute(sql)
+		DATABASE.commit()
+		cursor.close()	
+	except Exception:
+		return 1
+	if '-' in flag:
+		return 0
+	else:
+		return 2
+
+def DELETE_STUDENT(idno:int):
+	try:
+		sql:str = f"DELETE FROM `student` WHERE `id`={idno}"
+		cursor = DATABASE.cursor()
+		cursor.execute(sql)
+		DATABASE.commit()
+		cursor.close()
+	except Exception:
+		return False
+	return True
+
+def GETALL()->list:
+	sql:str = f"SELECT * FROM `student`"
+	cursor = DATABASE.cursor(dictionary=True)
+	cursor.execute(sql)
+	slist:list = cursor.fetchall()
+	cursor.close()
+	return slist
+
